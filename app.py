7f348567bd352d3f56fecddfdb28def10e728158
OliverRob06 <oliver.stewart.robinson@gmail.com>
2026-03-13 12:23:00 +0000

    Created Local Host Port For Testing

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = flask(__name__)
 @app.route('/info')
 def info():
     return render_template('info.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True, host = 0.0.0.0, port = 8000)
