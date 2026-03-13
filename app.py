f11f18fda645013b552e86fde5ee2588ce806dde
OliverRob06 <oliver.stewart.robinson@gmail.com>
2026-03-13 13:15:56 +0000

    Updating NavBar

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder = "html/template")
+app = Flask(__name__, template_folder = "html/template", static_folder = "images")
 
 
 @app.route('/')
