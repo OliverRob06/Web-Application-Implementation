53b257ac244572a99ec37c11affe9f4ee71d6940
OliverRob06 <oliver.stewart.robinson@gmail.com>
2026-03-13 12:49:20 +0000

    Updating Folders so Flask can Find base.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "html/template")
 
 
 @app.route('/')
@@ -15,4 +15,4 @@ def info():
 
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = '8000')
+    app.run(debug = True, host = '0.0.0.0.0', port = 8000)
