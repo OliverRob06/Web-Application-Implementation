3d106ec3e96885e66c42cd3e4ee0c91175be7312
OliverRob06 <oliver.stewart.robinson@gmail.com>
2026-03-13 12:35:05 +0000

    Updated Home Page For Template Testing

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/info')
 def info():
     return render_template('info.html')
