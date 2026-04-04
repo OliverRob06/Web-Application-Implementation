6d8d6e4977343da51e077f6c54196622493c0643
Jake <2637433@dundee.ac.uk>
2026-04-04 15:45:41 +0100

    fixed files to make templates work with css

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder = "html/template", static_folder = "images")
+app = Flask(__name__, template_folder = "html/template", static_folder = "static")
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/info')
 def info():
@@ -15,4 +23,4 @@ def info():
 
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0.0', port = 8000)
+    app.run(debug = True, host = '0.0.0.0', port = 8000)
