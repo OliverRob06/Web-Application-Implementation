d9afbdadb8011ebad23ea6d56417efcb1080f294
OliverRob06 <oliver.stewart.robinson@gmail.com>
2026-04-10 15:33:15 +0000

    fixed db

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ def account():
                 your_reviews.append({
                     "rating": r.get("rating"),
                     "content": r.get("content"),
-                    "movie_id": r.get("movie_id"),
+                    "movie_id": r.get("movie_id")
                 })
     except Exception as e:
         print(f"Review API Error: {e}")
